0f46605f7d39467acc3b009c058d0e399819449b
Stian Bakkane <stianba@gmail.com>
2015-08-25 11:38:51 +0200

    Converted to es6 npm compatible

---
+++ example/app.js
@@ -0,0 +1,4 @@
+const LinkPing = require ('../lib/link-ping');
+const linkPing = new LinkPing('body', {
+  include: ['.link'],
+});
---
+++ gulpfile.js
@@ -0,0 +1,32 @@
+const gulp = require('gulp');
+const browserify = require('browserify');
+const babelify = require('babelify');
+const source = require('vinyl-source-stream');
+const rename = require('gulp-rename');
+const bufferify = require('vinyl-buffer');
+
+gulp.task('bundle', function() {
+  'use strict';
+  
+  const bundler = browserify({
+    cache: {},
+    packageCache: {},
+    entries: ['./example/app.js'],
+    debug: true,
+  });
+
+  bundler.transform(babelify);
+  bundler.on('update', bundle);
+
+  function bundle() {
+    return bundler.bundle()
+      .pipe(source('./example/app.js'))
+      .pipe(bufferify())
+      .pipe(rename('bundle.js'))
+      .pipe(gulp.dest('./example'));
+  }
+
+  bundle();
+});
+
+gulp.task('default', ['bundle']);
